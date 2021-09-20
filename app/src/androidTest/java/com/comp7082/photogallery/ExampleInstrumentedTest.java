aa05df6499b45d034b138698f24abf653d066f09
Charles <charleshuang97@gmail.com>
2021-09-19 20:39:38 -0700

    added UI tests

--- app/src/androidTest/java/com/comp7082/photogallery/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/comp7082/photogallery/ExampleInstrumentedTest.java
@@ -1,10 +1,21 @@
 package com.comp7082.photogallery;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +27,29 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.comp7082.photogallery", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    // YOUR EMULATOR SCREEN HAS TO BE ON FOR THIS TO WORK
+    public void useUI(){
+        onView(withId(R.id.btnSearch)).perform(click());
+        onView(withId(R.id.etFromDateTime)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.etToDateTime)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.etKeywords)).perform(typeText("caption"), closeSoftKeyboard());
+        onView(withId(R.id.go)).perform(click());
+        onView(withId(R.id.etCaption)).check(matches(withText("caption")));
+        onView(withId(R.id.btnNext)).perform(click());
+        onView(withId(R.id.btnPrev)).perform(click());
     }
 }
